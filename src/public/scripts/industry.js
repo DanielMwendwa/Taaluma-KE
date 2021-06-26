c599c293dbdf331ca55516f5d78b00fbbc3f6237
DanielMwendwa <danielmwendwam@gmail.com>
2021-06-26 21:12:10 +0300

    updated scripts

--- src/public/scripts/industry.js
+++ src/public/scripts/industry.js
@@ -14,42 +14,43 @@ industry.preloadData = () => {
 
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
+    const queryStringObject = { code };
+    const requestPayload = { path: "/api/industry", method: "GET", queryStringObject };
 
     // Fetch the menu data.
-    queryStringObject = {code}
-    httpClient
-        .request({ path: "/api/industry", method: "GET", queryStringObject })
-        .then(({ statusCode, responsePayload }) => {
-            console.log(responsePayload)
-            responsePayload.careers.forEach((career => {
-                const itemElement = document.createElement("div");
-                itemElement.innerHTML = `<div class="container">
+    httpClient.request(requestPayload).then(({ statusCode, responsePayload }) => {
+        console.log(responsePayload);
+        responsePayload.careers.forEach((doc) => {
+            let title = doc.title;
+            let percent_employed = doc._percent_employed;
+
+            const itemElement = document.createElement("div");
+            itemElement.innerHTML = `<div class="container">
                     <div class="course">
                         <div class="preview">
                             <h6>Course</h6>
-                            <h2>Web Development</h2>
+                            <h2>Tags</h2>
                             <a href="#"> View All Chapters </a>
                         </div>
                         <div class="info">
                             <div class="progress-wrapper">
                                 <div class="progress"></div>
-                                <span class="progress-text">4/9 Challenges</span>
+                                <span class="progress-text">${percent_employed}% employed</span>
                             </div>
                             <h6>Chapter 4</h6>
-                            <h2>JAVASCRIPT, HTML & CSS</h2>
+                            <h2>${title}</h2>
                             <p class="p-trunc"> Master MongoDB Development for Web & Mobile Apps. CRUD Operations, Indexes, Aggregation Framework - All about MongoDB! </p>
-                            <button class="btn">Next Channel</button>
+                            <button class="btn">View Details</button>
                         </div>
                     </div>
                 </div>
-                `
-                industryContainer.appendChild(itemElement);
+                `;
+            industryContainer.appendChild(itemElement);
 
-                // const trunc = document.querySelector(".p-trunc");
-                // trunc.innerText = trunc.innerText.substring(0, 100) + "...";
-            }))
-
+            // const trunc = document.querySelector(".p-trunc");
+            // trunc.innerText = trunc.innerText.substring(0, 100) + "...";
         });
+    });
 };
 
 // Init user current page.
