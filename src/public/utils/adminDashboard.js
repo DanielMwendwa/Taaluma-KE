81f36b237d586a5b10b55a6c8bd39a9d33d7adea
DanielMwendwa <danielmwendwam@gmail.com>
2021-08-15 09:31:48 +0300

    rm courses section

--- src/public/utils/adminDashboard.js
+++ src/public/utils/adminDashboard.js
@@ -55,14 +55,6 @@ const adminSideBar = `<div class="navigation">
             </a>
         </li>
         <li>
-            <a href="/admin/dashboard/courses">
-                <span class="icon">
-                    <img width="30px" height="30px" src="/public/icons/book-reader-solid.png">
-                </span>
-                <span class="title">Course</span>
-            </a>
-        </li>
-        <li>
             <a href="/admin/sys/report">
                 <span class="icon">
                     <img width="30px" height="30px" src="/public/icons/server-solid.png">
