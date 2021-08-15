b22d9805b1740c2276099af2087e202ec393b17d
DanielMwendwa <danielmwendwam@gmail.com>
2021-08-16 01:16:19 +0300

    display students performance

--- src/public/scripts/adminDashboardSchoolResults.js
+++ src/public/scripts/adminDashboardSchoolResults.js
@@ -26,18 +26,57 @@ career.preloadData = () => {
             let entries = Object.entries(data);
             performances.push(entries);
         });
-        // console.log(performances)
 
-        let container = document.getElementById("perf-details")
+        const subjects = {
+            ENG: "English",
+            KIS: "Kiswahili",
+            MAT: "Mathematics",
+            BIO: "Biology",
+            PHY: "Physics",
+            CHE: "Chemistry",
+            GSC: "General Science",
+            HAG: "History",
+            GEO: "Geography",
+            CRE: "CRE",
+            IRE: "IRE",
+            HRE: "HRE",
+            HSC: "Home Science",
+            ARD: "Art and Design",
+            AGR: "Agriculture",
+            WW: "Wood Work",
+            MW: "Metal Work",
+            BC: "Building Construction",
+            PM: "Power Mechanics",
+            ECT: "Electricity",
+            DRD: "Drawing and Design",
+            AVT: "Aviation Technology",
+            CMP: "Computer Studies",
+            FRE: "French",
+            GER: "German",
+            ARB: "Arabic",
+            KSL: "Sign Language",
+            MUC: "Music",
+            BST: "Business Studies",
+        };
+
+        const studentDetails = {
+            index: "Index Number",
+            grade: "Grade",
+            year: "KCSE Year",
+        };
+
+        const performanceColumn = { ...subjects, ...studentDetails };
+        let container = document.getElementById("perf-details");
         performances.forEach((res) => {
-            let table = document.createElement("table"); let row;
+            let table = document.createElement("table");
+            let row;
             table.style.padding = "10px";
             table.style.border = "2px solid black";
             table.style.margin = "2px";
             res.forEach((el) => {
                 row = document.createElement("tr");
                 row.innerHTML = `
-                    <td>${el[0]}</td>
+                    <td>${performanceColumn[el[0]]}</td>
                     <td>${el[1]}</td>`;
                 table.appendChild(row);
             });
